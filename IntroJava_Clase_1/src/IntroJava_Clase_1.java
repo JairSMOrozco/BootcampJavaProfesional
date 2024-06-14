763add57f263b9404576f89b1ded04e77f5abc5e
Jair SM Orozco <jairoztricker7@gmail.com>
2024-06-13 22:10:38 -0600

    Cambio desde laptop

--- IntroJava_Clase_1/src/IntroJava_Clase_1.java
+++ IntroJava_Clase_1/src/IntroJava_Clase_1.java
@@ -1,5 +1,6 @@
 public class IntroJava_Clase_1 {
     public static void main(String[] args) {
+	System.out.println("Hola, esto es una prueba de que si funciona git");
         System.out.println("Hello world!");
     }
 }
